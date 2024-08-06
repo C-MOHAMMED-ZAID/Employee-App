6275d9d846ef822e5ed15eacec6d505eebe137c7
ZAID <mohammedzaid.connect@gmail.com>
2024-08-06 15:33:15 -0100

    Add files via upload

---
+++ Backend/models/Employee.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+let employeeSchema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String
+    },
+    empno:{
+        type:Number
+    },
+    phoneno:{
+        type:Number
+    },
+})
+module.exports=mongoose.model('Employee',employeeSchema)
+
+
+    
