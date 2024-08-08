8c0c29026dd92bcfd687f8424bf42c42f22a4441
ZAID <mohammedzaid.connect@gmail.com>
2024-08-08 03:01:24 -0100

    Update App.jsx

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
-import Home from './pages/Home'
-import EmployeeList from './pages/EmployeeList'
-import CreateEmployee from './pages/CreateEmployee'
-import EditEmployee from './pages/EditEmployee'
+import Home from './Components/pages/Home'
+import EmployeeList from './Components/pages/EmployeeList'
+import CreateEmployee from './Components/pages/CreateEmployee'
+import EditEmployee from './Components/pages/EditEmployee'
 import MenuBar from './Components/MenuBar'
-import Editsalary from './pages/EditSalary'
-import CreateSalary from './pages/CreateSalary'
-import SalaryList from './pages/SalaryList'
-import { BrowserRouter as Routes, Route } from 'react-router-dom'
+import Editsalary from './Components/pages/EditSalary'
+import CreateSalary from './Components/pages/CreateSalary'
+import SalaryList from './Components/pages/SalaryList'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div>
       <MenuBar></MenuBar>
-      
+      <Router>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/EmployeeList' element={<EmployeeList/>}></Route>
-          <Route path='/create-employee' element={<CreateEmployee/>}></Route>
+          <Route path='/CreateEmployee' element={<CreateEmployee/>}></Route>
           <Route path='/edit-employee/:id' element={<EditEmployee/>}></Route>
           <Route path='/create-salary' element={<CreateSalary/>}></Route>
           <Route path='/salary-list' element={<SalaryList/>}></Route>
           <Route path='/edit-salary/:id' element={<Editsalary/>}></Route>
         </Routes>
-      
-    
+      </Router>
+    <Footer></Footer>
     </div>
   )
 }
 
 export default App
+
